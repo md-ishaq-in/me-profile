026faf5d7ff340a6fae75830882e5e19aa87217a
Muhammad Ishaq <156215583+muhammadiishaq@users.noreply.github.com>
2025-10-06 04:54:36 +0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,27 @@
-import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';  // ✅ use modern import (no /serverless)
+// import { defineConfig } from 'astro/config';
+// import vercel from '@astrojs/vercel';  // ✅ use modern import (no /serverless)
 
-export default defineConfig({
-  output: 'server',
-  adapter: vercel(),
-});
-
-
-
-
-
-// // @ts-check
-// import { defineConfig } from "astro/config";
-
-// import tailwind from "@astrojs/tailwind";
-
-
-// // https://astro.build/config
 // export default defineConfig({
-//   site: "https://iemafzalhassan.in",
-//   output: "server",
-//   integrations: [tailwind()],
-//     devToolbar: {
-//     enabled: false
-//   }
+//   output: 'server',
+//   adapter: vercel(),
 // });
+
+
+
+// @ts-check
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
+
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://iemafzalhassan.in",
+  output: "server",
+  integrations: [tailwind()],
+    devToolbar: {
+    enabled: false
+  }
+});
 
 
