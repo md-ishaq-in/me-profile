eba8d90049379ec4d79f7b9907e78739618f6877
Muhammad Ishaq <devopsaws804@gmail.com>
2025-10-06 04:17:52 +0500

    new changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,31 @@
-// @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';  // ✅ Vercel adapter for SSR
 
-import tailwind from "@astrojs/tailwind";
-
-
-// https://astro.build/config
+// https://docs.astro.build/en/configuration-reference
 export default defineConfig({
-  site: "https://iemafzalhassan.in",
-  output: "server",
-  integrations: [tailwind()],
-    devToolbar: {
-    enabled: false
-  }
+  output: 'server',   // Required for backend functionality
+  adapter: vercel(),  // Tell Astro to use the Vercel runtime
 });
+
+
+
+
+
+
+// // @ts-check
+// import { defineConfig } from "astro/config";
+
+// import tailwind from "@astrojs/tailwind";
+
+
+// // https://astro.build/config
+// export default defineConfig({
+//   site: "https://iemafzalhassan.in",
+//   output: "server",
+//   integrations: [tailwind()],
+//     devToolbar: {
+//     enabled: false
+//   }
+// });
 
 
