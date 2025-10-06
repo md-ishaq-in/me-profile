cfd99d57e943d8f66b57f461accb9405645b4877
Muhammad Ishaq <156215583+muhammadiishaq@users.noreply.github.com>
2025-10-07 00:48:54 +0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
-  site: "https://iemafzalhassan.in",
+  site: "https://muhammadiishaq.github.io/portfolio-test/", // your GitHub Pages URL
+  base: "/portfolio-test/", // your repo name
   output: "static",
   integrations: [tailwind()],
 });
